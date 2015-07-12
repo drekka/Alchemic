e0cb233844d7935ecff35014d259a5e8e9d03d74
Derek Clarkson <d4rkf1br3@gmail.com>
2015-07-13 00:14:10 +1000

    Updatng doco.

--- alchemic/ALCContext.h
+++ alchemic/ALCContext.h
@@ -26,19 +26,35 @@
 
 /**
  Adds an additional initialisation strategy to the built in ones.
- Strategies are run in reverse order from last registered through to the builtin ones.
+ Strategies are run in reverse order from last registered through to the builtin ones when attempting to instantiate an object.
+ 
+ @param initialisationStrategyClass the class to be added.
  */
 -(void) addInitStrategy:(Class __nonnull) initialisationStrategyClass;
 
+/**
+ Adds an dependency post processor.
+ 
+ @discussion dependency post processors are executed after depedencies have been resolve and before their values are accessed for injection.
+ */
 -(void) addDependencyPostProcessor:(id<ALCDependencyPostProcessor> __nonnull) postProcessor;
 
 /**
- Adds a ALCObjectFactory to the list of object factories. Factories are checked in reverse order. The last registered object factory is the one asked first for an object.
+ Adds a ALCObjectFactory to the list of object factories. 
+ 
+ @discussion Factories are checked in reverse order. The last registered object factory is the one asked first for an object.
+ 
+ @param objectFactory the factory to add.
  */
 -(void) addObjectFactory:(id<ALCObjectFactory> __nonnull) objectFactory;
 
 #pragma mark - Lifecycle
 
+/**
+ Starts the context.
+ 
+ @discussion After scanning the runtime for Alchemic registrations, this called to start the context. This involves resolving all dependencies, instantiating all registered singletons and finally injecting any dependencies of those singletons.
+ */
 -(void) start;
 
 #pragma mark - Dependencies
@@ -50,6 +66,13 @@
  */
 -(void) resolveDependency:(ALCDependency __nonnull *) dependency;
 
+/**
+ Access point for objects which need to have dependencies injected. 
+ 
+ @discussion This checks the model against the model. If a class builder is found which matches the class and protocols of the passed object, it is used to inject any listed dependencies into the object.
+ 
+ @param object the object which needs dependencies injected.
+ */
 -(void) injectDependencies:(id __nonnull) object;
 
 @end
--- alchemic/ALCMacros.h
+++ alchemic/ALCMacros.h
@@ -12,33 +12,91 @@
 
 #pragma mark - Defining objects
 
+/**
+ Tells Alchemic to set a custom name on a registration.
+ */
 #define ACAsName(_objectName) [ALCAsName asNameWithName:_objectName]
 
+/**
+ Defines the class or factory method as a factory.
+ 
+ @discussion Factories create a new instance of an object every time they are accessed.
+ 
+ @param _objectName the name to set on the registration.
+ */
 #define ACIsFactory [[ALCIsFactory alloc] init]
 
+/**
+ When there is more than one candidate object for a dependency, Primary objects are used first. 
+ 
+ @discussion This is mainly used for a couple of situations. Firstly where there are a number of candiates and you don't want to use names to define a default. Secondly during unit testing, this can be used to set registrations in unit test code as overrides to the app's instances.
+ */
 #define ACIsPrimary [[ALCIsPrimary alloc] init]
 
 #pragma mark - Dependency matching
 
+/**
+ Used when we want to locate dependencies based on a class.
+ 
+ @param _className the name of the class to search for.
+ */
 #define ACWithClass(_className) [ALCQualifier qualifierWithValue:[_className class]]
 
+/**
+ Used to search for dependencies based on a protocol that a class implements.
+ 
+ @param _ProtocolName the name of the protocol.
+ */
 #define ACWithProtocol(_protocolName) [ALCQualifier qualifierWithValue:@protocol(_protocolName)]
 
+/**
+ Used to search for builders based on the names given to them.
+ 
+ @param _objectName The name to search on.
+ */
 #define ACWithName(_objectName) [ALCQualifier qualifierWithValue:_objectName]
 
+/**
+ When defining a method which builds an object, this tells the builder what class is expected to be returned from the method.
+ 
+ @discussion The class declared by this macro is used for searching when resolving dependencies.
+ 
+ _returnType The class that is to be returned.
+ */
 #define ACReturnType(_returnType) [ALCReturnType returnTypeWithClass:[_returnType class]]
 
+/**
+ The selector of a method in the classes that will be used to build objects.
+
+ @discussion In the ACRegister(...) macro, the rest of the ACWith... arguments must match the arguments to this selector, in order. This allows Alchemic to then locate the dependencies for those arguments. If an argument needs more than one ACWith... to locate the dependency, they should be wrapped in an Objective-C array.
+
+ @param _methodSelector the selector to call when a value is needed.
+ */
 #define ACFactorySelector(_methodSelector) [ALCMethodSelector methodSelector:@selector(_methodSelector)]
 
+/**
+ When declaring a dependency in a class, this defines the name of the variable that the value will be injected into.
+ 
+ @param _variableName the name of the variable. If a property is the target, this can either be the name of the property or the variable behind the property.
+ */
 #define ACIntoVariable(_variableName) [ALCIntoVariable intoVariableWithName:_alchemic_toNSString(_variableName)]
 
 #pragma mark - Injection
 
+/**
+ If you need to manually resolve and inject values into an object, then call this macro passing it the object to be injected.
+ 
+ @param object the object whose dependencies need to be resolved and injected.
+ */
 #define ACInjectDependencies(object) [[ALCAlchemic mainContext] injectDependencies:object]
 
 #pragma mark - Registering
 
-// All registration methods make use of the same signature.
+/**
+ Register a class or method as a source of objects.
+ 
+ @discussion This is the main macro for setting up objects within Alchemic. It take a number of other macros as 
+ */
 #define ACRegister(...) \
 +(void) _alchemic_concat(ALCHEMIC_METHOD_PREFIX, _registerClassBuilder):(ALCClassBuilder *) classBuilder { \
 [[ALCAlchemic mainContext] registerClassBuilder:classBuilder, ## __VA_ARGS__, nil]; \
