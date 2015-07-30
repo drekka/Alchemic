6aa293f74428cd84a03f2359228bb420f17a9ec1
Derek Clarkson <d4rkf1br3@gmail.com>
2015-07-31 00:39:54 +1000

    More testing

--- alchemicTests/MethodSingletonsIntegrationTests.m
+++ alchemicTests/MethodSingletonsIntegrationTests.m
@@ -13,12 +13,20 @@
 @interface MSParentClass : NSObject
 @end
 
-@implementation MSParentClass
+@implementation MSParentClass {
+	int count;
+}
 
 AcMethod(NSString, createAString, AcWithName(@"abc"))
-
 -(NSString *) createAString {
-	return @"hello world";
+	STLog(self, @"----> Creating a object with count %i", count);
+	return [NSString stringWithFormat:@"hello world %i", count++];
+}
+
+AcMethod(NSString, createAStringFromAString:, AcArg(NSString, AcName(@"abc")), AcWithName(@"def"))
+-(NSString *) createAStringFromAString:(NSString *) aString {
+	STLog(self, @"----> Creating a object with text %@", aString);
+	return [aString stringByAppendingString:@" again !"];
 }
 
 @end
@@ -27,17 +35,32 @@ AcMethod(NSString, createAString, AcWithName(@"abc"))
 @end
 
 @implementation MethodSingletonIntegrationTests {
-	NSString *_aString;
+	NSString *_aString1;
+	NSString *_aString2;
+	NSString *_aString3;
 }
 
-AcInject(_aString, AcName(@"abc"))
+AcInject(_aString1, AcName(@"abc"))
+AcInject(_aString2, AcName(@"abc"))
+AcInject(_aString3, AcName(@"def"))
 
 -(void) testCreatingASingleton {
+	[self setupRealContext];
+	STStartLogging(ALCHEMIC_LOG);
+	STStartLogging(@"[MSParentClass]");
+	STStartLogging(@"[MethodSingletonIntegrationTests]");
+	[self addClassesToContext:@[[MSParentClass class], [MethodSingletonIntegrationTests class]]];
+	AcInjectDependencies(self);
+	XCTAssertEqualObjects(@"hello world 0", _aString1);
+	XCTAssertEqualObjects(@"hello world 0", _aString2);
+}
+
+-(void) testCreatingASingletonWithAnArg {
 	[self setupRealContext];
 	STStartLogging(@"[MethodSingletonIntegrationTests]");
 	[self addClassesToContext:@[[MSParentClass class], [MethodSingletonIntegrationTests class]]];
 	AcInjectDependencies(self);
-	XCTAssertEqualObjects(@"hello world", _aString);
+	XCTAssertEqualObjects(@"hello world 0 again !", _aString3);
 }
 
 @end
