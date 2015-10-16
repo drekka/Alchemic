78829e8cc779da81724ce6e4899cba7920e346c5
Derek Clarkson <dclarkson@odecee.com.au>
2015-10-16 17:58:47 +1100

    Testing out adding Swift support.

---
+++ alchemicTests/ALCContextSTests.swift
@@ -0,0 +1,19 @@
+//
+//  ALCContextSTests.swift
+//  alchemic
+//
+//  Created by Derek Clarkson on 16/10/2015.
+//  Copyright © 2015 Derek Clarkson. All rights reserved.
+//
+
+import XCTest
+import Alchemic
+
+class ALCContextSTests: XCTestCase {
+
+    func testContext() {
+        let context = ALCContext();
+        XCTAssertNotNil(context);
+    }
+
+}
