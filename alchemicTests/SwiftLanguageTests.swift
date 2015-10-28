95cf419643acd09dc4d3d25d5fad0dbd8841f517
Derek Clarkson <dclarkson@odecee.com.au>
2015-10-28 18:18:42 +1100

    MOre tests

--- alchemicTests/SwiftLanguageTests.swift
+++ alchemicTests/SwiftLanguageTests.swift
@@ -1,43 +1,47 @@
-//
-//  SwiftLanguageTests.swift
-//  alchemic
-//
-//  Created by Derek Clarkson on 28/10/2015.
-//  Copyright © 2015 Derek Clarkson. All rights reserved.
-//
 
 // Tests related to understanding the Swift language.
 
+import Foundation
 import XCTest
+
 
 class SwiftLanguageTests: XCTestCase {
 
     class TestClass {
+        var name:String?
         var xyz:String?
     }
 
-    func createTestClass() -> TestClass {
-        let obj = TestClass()
-        obj.xyz = "abc"
-        return obj
+    func testAccessingPropertiesOfAnyObjectInstancesReturnsNils() {
+        let instance = TestClass()
+        instance.xyz = "xyz"
+        instance.name = "name"
+
+        let typeAnyObject = instance as AnyObject
+
+        // Won't compile because 'xyz' is an unknown property in any class.
+        //XCTAssertEqual("xyz", typeAnyObject.xyz)
+
+        // Will compile because 'name' is a property of NSException
+        // But returns a nil when accessed.
+        XCTAssertEqual("name", typeAnyObject.name)
+
     }
 
-    func testUsingAGenericTypeDerivedFromAClassArgument() {
-
-        func getObj<T>(arg:T.Type) -> T {
-            return createTestClass()  as! T
-        }
-
-        let unknownTypeObj = getObj(TestClass.self)
-
-        let theName = unknownTypeObj.xyz
-        XCTAssertEqual("abc", theName)
-    }
 
     func testUsingAGenericTypeDerivedFromTheReturnType() {
 
+        func createTestClass() -> TestClass {
+            let obj = TestClass()
+            obj.xyz = "abc"
+            return obj
+        }
+
         func getObj<T>() -> T {
-            return createTestClass() as! T
+            //var obj2 = createTestClass()
+            let obj = TestClass()
+            obj.xyz = "abc"
+            return obj as! T
         }
 
         let unknownTypeObj:TestClass = getObj()
@@ -46,5 +50,19 @@ class SwiftLanguageTests: XCTestCase {
         XCTAssertEqual("abc", theName)
     }
 
+    func testUsingAGenericTypeDerivedFromAClassArgument() {
+
+        func getObj<T>(arg:T.Type) -> T {
+            let obj = TestClass()
+            obj.xyz = "abc"
+            return obj as! T
+            //return createTestClass() as! T
+        }
+
+        let unknownTypeObj = getObj(TestClass.self)
+
+        let theName = unknownTypeObj.xyz
+        XCTAssertEqual("abc", theName)
+    }
 
 }
