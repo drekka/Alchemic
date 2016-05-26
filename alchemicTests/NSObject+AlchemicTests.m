c237e7044d5441008497511ecdb2ad446cb48405
Derek Clarkson <d4rkf1br3@gmail.com>
2016-05-26 21:31:05 +1000

    ADding more tests.

---
+++ alchemicTests/NSObject+AlchemicTests.m
@@ -0,0 +1,26 @@
+//
+//  NSObject+AlchemicTests.m
+//  Alchemic
+//
+//  Created by Derek Clarkson on 25/05/2016.
+//  Copyright © 2016 Derek Clarkson. All rights reserved.
+//
+
+@import XCTest;
+@import Alchemic;
+@import Alchemic.Private;
+
+@interface NSObject_AlchemicTests : XCTestCase
+
+@end
+
+@implementation NSObject_AlchemicTests
+
+-(void) testInstanceInvokeSelectorWithArguments {
+    
+    NSString *obj = @"abc";
+    
+    
+}
+
+@end
