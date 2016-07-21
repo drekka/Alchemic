3dfabfe4bf1653e996bda57cce898e7466446727
Derek Clarkson <d4rkf1br3@gmail.com>
2016-07-21 15:03:01 +1000

    Tests for bundles.

--- alchemicTests/NSBundle+AlchemicTests.m
+++ alchemicTests/NSBundle+AlchemicTests.m
@@ -9,10 +9,43 @@
 @import XCTest;
 @import Alchemic;
 @import Alchemic.Private;
+@import OCMock;
 
 @interface NSBundle_AlchemicTests : XCTestCase
 @end
 
 @implementation NSBundle_AlchemicTests
+
+-(void) testScanWithProcessors {
+    
+    NSBundle *thisBundle = [NSBundle bundleForClass:[self class]];
+    id contextMock = OCMProtocolMock(@protocol(ALCContext));
+    
+    id canProcessMock = OCMProtocolMock(@protocol(ALCClassProcessor));
+    OCMStub([canProcessMock canProcessClass:OCMOCK_ANY]).andReturn(YES);
+    OCMExpect([canProcessMock processClass:OCMOCK_ANY withContext:contextMock]);
+    
+    id cantProcessMock = OCMProtocolMock(@protocol(ALCClassProcessor));
+    
+    __block unsigned int foundClasses = 0;
+    OCMStub([cantProcessMock canProcessClass:[OCMArg checkWithBlock:^BOOL(id obj) {
+        foundClasses++;
+        return YES;
+    }]]).andReturn(NO);
+    
+    
+    [thisBundle scanWithProcessors:@[
+                                     canProcessMock,
+                                     cantProcessMock
+                                     ]
+                           context:contextMock];
+    
+    unsigned int nbrClasses = 0;
+    __unused const char** classes = objc_copyClassNamesForImage([[thisBundle executablePath] UTF8String], &nbrClasses);
+    
+    OCMVerifyAll(canProcessMock);
+    XCTAssertEqual(nbrClasses, foundClasses);
+    
+}
 
 @end
