a6b947eba1cb17c05bfaabcbdbb42a2108dd9359
Derek Clarkson <d4rkf1br3@gmail.com>
2016-07-25 00:30:12 +1000

    Fixing error traps in factories.
    More tests.

--- alchemic/ALCClassObjectFactory.m
+++ alchemic/ALCClassObjectFactory.m
@@ -24,16 +24,16 @@
 
 
 @implementation ALCClassObjectFactory {
-    
+
     BOOL _resolved;
     BOOL _checkingReadyStatus;
-    
+
     NSMutableArray<id<ALCDependency>> *_dependencies;
-    
-    // Populated so we can reinject values if dependencies update.
+
+    // Populated if there are transient dependencies so we can reinject values if they update.
     // Using a weak based hashtable so we don't have to maintain it.
     NSHashTable *_injectedObjectHistory;
-    
+
 }
 
 @synthesize initializer = _initializer;
@@ -44,17 +44,8 @@
     self = [super initWithClass:objectClass];
     if (self) {
         _dependencies = [[NSMutableArray alloc] init];
-        _injectedObjectHistory = [[NSHashTable alloc] initWithOptions:NSHashTableWeakMemory capacity:0];
     }
     return self;
-}
-
--(void)configureWithOption:(id)option model:(id<ALCModel>) model {
-    if (_initializer && [option isKindOfClass:[ALCIsReference class]]) {
-        throwException(IllegalArgument, @"Factories with initializers cannot be set to reference external objects");
-    } else {
-        [super configureWithOption:option model:model];
-    }
 }
 
 -(ALCVariableDependency *) registerVariableDependency:(Ivar) variable
@@ -68,17 +59,22 @@
 }
 
 -(void)resolveWithStack:(NSMutableArray<id<ALCResolvable>> *)resolvingStack model:(id<ALCModel>) model {
-    
+
     STLog(self.objectClass, @"Resolving class factory %@", NSStringFromClass(self.objectClass));
-    
+
+    // Validate we are not trying to specify an intializer for a reference factory.
+    if (_initializer && self.factoryType == ALCFactoryTypeReference) {
+        throwException(IllegalArgument, @"Reference factories cannot have initializers");
+    }
+
     AcWeakSelf;
     [self resolveWithResolvingStack:resolvingStack
                        resolvedFlag:&_resolved
                               block:^{
-                                  
+
                                   AcStrongSelf;
                                   [strongSelf->_initializer resolveWithStack:resolvingStack model:model];
-                                  
+
                                   STLog(strongSelf.objectClass, @"Resolving %i injections into a %@", strongSelf->_dependencies.count, NSStringFromClass(strongSelf.objectClass));
                                   for (ALCVariableDependency *ref in strongSelf->_dependencies) {
                                       [resolvingStack addObject:ref];
@@ -86,25 +82,41 @@
                                       [resolvingStack removeLastObject];
                                   }
                               }];
-    
-    // Check for the need to watch notifications.
+
+    // Find the first transient dependency and setup watching notifications.
     for (ALCVariableDependency *dependency in _dependencies) {
         if (dependency.transient) {
-            [self setDependencyUpdateObserverWithBlock:^(NSNotification *notification) {
-                AcStrongSelf;
-                id sourceObjectFactory = notification.object;
-                for (ALCVariableDependency *variableDependency in strongSelf->_dependencies) {
-                    if (variableDependency.transient && [variableDependency referencesObjectFactory:sourceObjectFactory]) {
-                        for (id object in strongSelf->_injectedObjectHistory) {
-                            STLog(strongSelf.objectClass, @"Updating dependency value %@", variableDependency);
-                            [strongSelf injectObject:object dependency:variableDependency];
-                        }
-                    }
-                }
-            }];
+            [self setUpTransientWatch];
             break;
         }
     }
+}
+
+-(void) setUpTransientWatch {
+    // If there is no history storage then add it.
+    if (!_injectedObjectHistory) {
+        _injectedObjectHistory = [[NSHashTable alloc] initWithOptions:NSHashTableWeakMemory capacity:0];
+    }
+
+    // Set up the dependency to watch the factories it resolves.
+    AcWeakSelf;
+    [self setDependencyUpdateObserverWithBlock:^(NSNotification *notification) {
+
+        AcStrongSelf;
+        id sourceObjectFactory = notification.object;
+
+        // Loop through all dependencies and check to see if they are watching the factory that had it's value changed.
+        for (ALCVariableDependency *variableDependency in strongSelf->_dependencies) {
+            if (variableDependency.transient && [variableDependency referencesObjectFactory:sourceObjectFactory]) {
+
+                // Loop through all the objects this factory has injected and update the dependency in them.
+                for (id object in strongSelf->_injectedObjectHistory) {
+                    STLog(strongSelf.objectClass, @"Updating dependency value %@", variableDependency);
+                    [strongSelf injectObject:object dependency:variableDependency];
+                }
+            }
+        }
+    }];
 }
 
 -(BOOL) isReady {
@@ -133,17 +145,17 @@
 #pragma mark - Tasks
 
 -(void) injectDependencies:(id) object {
-    
+
     STLog(self.objectClass, @"Injecting dependencies into a %@", NSStringFromClass(self.objectClass));
-    
+
     // Perform injections.
     for (ALCVariableDependency *dep in _dependencies) {
         [self injectObject:object dependency:dep];
     }
-    
+
     // Store a weak reference.
     [_injectedObjectHistory addObject:object];
-    
+
     // Notify of injection completion.
     if ([object respondsToSelector:@selector(alchemicDidInjectDependencies)]) {
         STLog(self, @"Telling %@ it's injections have finished", object);
--- alchemicTests/ALCClassObjectFactoryTests.m
+++ alchemicTests/ALCClassObjectFactoryTests.m
@@ -9,11 +9,69 @@
 @import XCTest;
 @import Alchemic;
 @import Alchemic.Private;
+@import OCMock;
+@import ObjectiveC;
 
 @interface ALCClassObjectFactoryTests : XCTestCase
 @end
 
-@implementation ALCClassObjectFactoryTests
+@implementation ALCClassObjectFactoryTests {
+    ALCClassObjectFactory *_factory;
+    id _mockModel;
+}
 
+-(void)setUp {
+    _factory = [[ALCClassObjectFactory alloc] initWithClass:[NSString class]];
+    _mockModel = OCMProtocolMock(@protocol(ALCModel));
+}
+
+-(void) testConfigureWithOptionModelSuperCall {
+    [_factory configureWithOption:AcNillable model:_mockModel];
+    XCTAssertTrue(_factory.isNillable);
+}
+
+-(void) testConfigureWithOptionModelReference {
+    [_factory configureWithOption:AcReference model:_mockModel];
+    XCTAssertEqual(ALCFactoryTypeReference, _factory.factoryType);
+}
+
+-(void) testResolveWithStackModelThrowsWhenIncorrectConfig {
+
+    // Setup initializer on reference factory.
+    [_factory configureWithOption:AcReference model:_mockModel];
+    __unused id _ = [[ALCClassObjectFactoryInitializer alloc] initWithObjectFactory:_factory initializer:@selector(init) args:nil];
+
+    NSMutableArray *stack = [[NSMutableArray alloc] init];
+    XCTAssertThrowsSpecific([_factory resolveWithStack:stack model:_mockModel], AlchemicIllegalArgumentException);
+}
+
+-(void) testResolveWithStackModelResolvesInitializer {
+
+    NSMutableArray *stack = [[NSMutableArray alloc] init];
+
+    // Setup initializer on reference factory.
+    id mockInitializer = OCMClassMock([ALCClassObjectFactoryInitializer class]);
+    OCMExpect([mockInitializer resolveWithStack:stack model:_mockModel]);
+
+    [_factory resolveWithStack:stack model:_mockModel];
+
+    OCMVerify(mockInitializer);
+}
+
+-(void) testResolveWithStackModelResolvesDependencies {
+
+    NSMutableArray *stack = [[NSMutableArray alloc] init];
+    id mockInjector = OCMProtocolMock(@protocol(ALCInjector));
+    OCMExpect([mockInjector resolveWithStack:stack model:_mockModel]);
+
+    Ivar ivar = class_getInstanceVariable([self class], "_mockModel");
+    __unused id _ = [_factory registerVariableDependency:ivar
+                                                injector:mockInjector
+                                                withName:@"mockModel"];
+
+    [_factory resolveWithStack:stack model:_mockModel];
+
+    OCMVerifyAll(mockInjector);
+}
 
 @end
