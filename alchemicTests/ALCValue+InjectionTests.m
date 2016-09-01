a177e9dd5cb569a7e0f868eb316e30d0cabb06c2
Derek Clarkson <d4rkf1br3@gmail.com>
2016-09-01 17:45:25 +1000

    More tests for injections.

--- alchemicTests/ALCValue+InjectionTests.m
+++ alchemicTests/ALCValue+InjectionTests.m
@@ -12,36 +12,33 @@
 @import ObjectiveC;
 
 @interface ALCValue_InjectionTests : XCTestCase
+
+@property (nonatomic, assign) BOOL aBool;
+@property (nonatomic, assign) signed char aChar;
+@property (nonatomic, assign) char * aCharPointer;
+@property (nonatomic, assign) double aDouble;
+@property (nonatomic, assign) float aFloat;
+@property (nonatomic, assign) signed int aInt;
+@property (nonatomic, assign) signed long aLong;
+@property (nonatomic, assign) signed long long aLongLong;
+@property (nonatomic, assign) signed short aShort;
+@property (nonatomic, assign) unsigned char aUChar;
+@property (nonatomic, assign) unsigned int aUInt;
+@property (nonatomic, assign) unsigned long aULong;
+@property (nonatomic, assign) unsigned long long aULongLong;
+@property (nonatomic, assign) unsigned short aUShort;
+
+@property (nonatomic, strong) NSNumber *aNumber;
+
+@property (nonatomic, assign) CGSize size;
+@property (nonatomic, assign) CGPoint point;
+@property (nonatomic, assign) CGRect rect;
+
 @end
 
-@implementation ALCValue_InjectionTests {
-    BOOL aBool;
-    signed char aChar;
-    char * aCharPointer;
-    double aDouble;
-    float aFloat;
-    signed int aInt;
-    signed long aLong;
-    signed long long aLongLong;
-    signed short aShort;
-    unsigned char aUChar;
-    unsigned int aUInt;
-    unsigned long aULong;
-    unsigned long long aULongLong;
-    unsigned short aUShort;
-    
-    NSNumber *aNumber;
-    NSArray<NSNumber *> *aArray;
-    
-    id idOnly;
-    id<NSCopying> idWithProtocol;
-    NSNumber<AlchemicAware> *classAndProtocol;
-    
-    CGSize size;
-    CGPoint point;
-    CGRect rect;
-    
-}
+@implementation ALCValue_InjectionTests
+
+#pragma mark - Variable injections
 
 #define testScalarVariableInjectionTest(ivarName, ivarType, expectedValue) \
 -(void) testVariableInjection_ ## ivarName { \
@@ -55,28 +52,28 @@ inj(self, ivar); \
 XCTAssertEqual(ivarName, expectedValue); \
 }
 
-testScalarVariableInjectionTest(aBool, BOOL, YES)
-testScalarVariableInjectionTest(aDouble, double, 5.12345678)
-testScalarVariableInjectionTest(aFloat, float, 1.2f)
-testScalarVariableInjectionTest(aInt, int, 5)
-testScalarVariableInjectionTest(aLong, long, 5)
-testScalarVariableInjectionTest(aLongLong, long long, 5)
-testScalarVariableInjectionTest(aShort, short, 5)
-testScalarVariableInjectionTest(aUChar, unsigned char, 'c')
-testScalarVariableInjectionTest(aUInt, unsigned int, 5u)
-testScalarVariableInjectionTest(aULong, unsigned long, 5u)
-testScalarVariableInjectionTest(aULongLong, unsigned long long, 5u)
-testScalarVariableInjectionTest(aUShort, unsigned short, 5u)
+testScalarVariableInjectionTest(_aBool, BOOL, YES)
+testScalarVariableInjectionTest(_aDouble, double, 5.12345678)
+testScalarVariableInjectionTest(_aFloat, float, 1.2f)
+testScalarVariableInjectionTest(_aInt, int, 5)
+testScalarVariableInjectionTest(_aLong, long, 5)
+testScalarVariableInjectionTest(_aLongLong, long long, 5)
+testScalarVariableInjectionTest(_aShort, short, 5)
+testScalarVariableInjectionTest(_aUChar, unsigned char, 'c')
+testScalarVariableInjectionTest(_aUInt, unsigned int, 5u)
+testScalarVariableInjectionTest(_aULong, unsigned long, 5u)
+testScalarVariableInjectionTest(_aULongLong, unsigned long long, 5u)
+testScalarVariableInjectionTest(_aUShort, unsigned short, 5u)
 
 -(void) testVariableInjection_aCharPointer {
-    Ivar ivar = class_getInstanceVariable([self class], "aCharPointer");
+    Ivar ivar = class_getInstanceVariable([self class], "_aCharPointer");
     const char *encoding = ivar_getTypeEncoding(ivar);
     char *value = "abc";
     NSValue *nsValue = [NSValue valueWithBytes:&value objCType:encoding];
     ALCValue *alcValue = [[ALCType typeForIvar:ivar] withValue:nsValue completion:NULL];
     ALCVariableInjectorBlock inj = [alcValue variableInjector];
     inj(self, ivar);
-    XCTAssertTrue(strcmp("abc", aCharPointer) == 0);
+    XCTAssertTrue(strcmp("abc", _aCharPointer) == 0);
 }
 
 #define testScalarStructVariableInjectionTest(ivarName, ivarType, expectedValue, compareFunction) \
@@ -91,16 +88,60 @@ inj(self, ivar); \
 XCTAssertTrue(compareFunction(ivarName, expectedValue)); \
 }
 
-testScalarStructVariableInjectionTest(size, CGSize, CGSizeMake(1.0f, 2.0f), CGSizeEqualToSize)
-testScalarStructVariableInjectionTest(point, CGPoint, CGPointMake(1.0f, 2.0f), CGPointEqualToPoint)
-testScalarStructVariableInjectionTest(rect, CGRect, CGRectMake(1.0f, 2.0f, 3.0f, 4.0f), CGRectEqualToRect)
+testScalarStructVariableInjectionTest(_size, CGSize, CGSizeMake(1.0f, 2.0f), CGSizeEqualToSize)
+testScalarStructVariableInjectionTest(_point, CGPoint, CGPointMake(1.0f, 2.0f), CGPointEqualToPoint)
+testScalarStructVariableInjectionTest(_rect, CGRect, CGRectMake(1.0f, 2.0f, 3.0f, 4.0f), CGRectEqualToRect)
 
 -(void) testVariableInjection_aNumber {
-    Ivar ivar = class_getInstanceVariable([self class], "aNumber");
+    Ivar ivar = class_getInstanceVariable([self class], "_aNumber");
     ALCValue *alcValue = [[ALCType typeForIvar:ivar] withValue:@5 completion:NULL];
     ALCVariableInjectorBlock inj = [alcValue variableInjector];
     inj(self, ivar);
-    XCTAssertEqualObjects(@5, aNumber);
+    XCTAssertEqualObjects(@5, _aNumber);
+}
+
+#pragma mark - Invocation injections
+
+#define testScalarInvocationInjectionTest(varName, ivarType, methodSelector, expectedValue) \
+-(void) testInvocationInjection_ ## varName { \
+Method method = class_getInstanceMethod([self class], @selector(methodSelector)); \
+NSMethodSignature *sig = [NSMethodSignature signatureWithObjCTypes:method_getTypeEncoding(method)]; \
+NSInvocation *inv = [NSInvocation invocationWithMethodSignature:sig]; \
+inv.selector = @selector(methodSelector); \
+ivarType value = expectedValue; \
+NSValue *nsValue = [NSValue valueWithBytes:&value objCType:@encode(ivarType)]; \
+ALCValue *alcValue = [[ALCType typeWithEncoding:@encode(ivarType)] withValue:nsValue completion:NULL]; \
+ALCInvocationInjectorBlock inj = [alcValue invocationInjector]; \
+inj(inv, 0); \
+[inv invokeWithTarget:self]; \
+XCTAssertEqual(_ ## varName, expectedValue); \
+}
+
+testScalarInvocationInjectionTest(aBool, BOOL, setABool:, YES)
+testScalarInvocationInjectionTest(aDouble, double, setADouble:, 5.12345678)
+testScalarInvocationInjectionTest(aFloat, float, setAFloat:, 1.2f)
+testScalarInvocationInjectionTest(aInt, int, setAInt:, 5)
+testScalarInvocationInjectionTest(aLong, long, setALong:, 5)
+testScalarInvocationInjectionTest(aLongLong, long long, setALongLong:, 5)
+testScalarInvocationInjectionTest(aShort, short, setAShort:, 5)
+testScalarInvocationInjectionTest(aUChar, unsigned char, setAUChar:, 'c')
+testScalarInvocationInjectionTest(aUInt, unsigned int, setAUInt:, 5u)
+testScalarInvocationInjectionTest(aULong, unsigned long, setAULong:, 5u)
+testScalarInvocationInjectionTest(aULongLong, unsigned long long, setAULongLong:, 5u)
+testScalarInvocationInjectionTest(aUShort, unsigned short, setAUShort:, 5u)
+
+-(void) testInvocationInjection_aCharPointer {
+    Method method = class_getInstanceMethod([self class], @selector(setACharPointer:));
+    NSMethodSignature *sig = [NSMethodSignature signatureWithObjCTypes:method_getTypeEncoding(method)];
+    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:sig];
+    inv.selector = @selector(setACharPointer:);
+    char *value = "abc";
+    NSValue *nsValue = [NSValue valueWithBytes:&value objCType:"*"];
+    ALCValue *alcValue = [[ALCType typeWithEncoding:"*"] withValue:nsValue completion:NULL];
+    ALCInvocationInjectorBlock inj = [alcValue invocationInjector];
+    inj(inv, 0);
+    [inv invokeWithTarget:self];
+    XCTAssertTrue(strcmp(_aCharPointer, "abc") == 0);
 }
 
 @end
